2d5a5a7b84093c14a0671ba530360c41f0bd3817
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-04-10 18:34:46 +0530

    Delete gmail.php

--- gmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $header='From: sajjad@techsajjad.tk' . "\r\n" .'Reply-To: info@techsajjad.tk';
-    if (mail("order-update@techsajjad.tk", "Test", "Hello this mail is from sajjad to order-update@techsajjad.tk",$header)) {
-        echo "success";
-    }
-    else{
-        echo "failure";
-    }
-?>
